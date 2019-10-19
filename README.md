5a8f23267dff907c54071a7469ec0852bef21ec4
ViggieSmalls <victormovileanu@gmail.com>
2019-10-19 18:34:24 +0200

    tutorial part 2

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 # Useful Links
 * https://django-oauth-toolkit.readthedocs.io/en/latest/tutorial/tutorial_01.html
+* https://django-oauth-toolkit.readthedocs.io/en/latest/tutorial/tutorial_02.html
+
+
+# Links for testing
+* http://django-oauth-toolkit.herokuapp.com/consumer/
+* http://django-oauth-toolkit.herokuapp.com/consumer/client/
--- django_sandbox/urls.py
+++ django_sandbox/urls.py
@@ -13,13 +13,43 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+
+import oauth2_provider.views as oauth2_views
+
+# OAuth2 provider endpoints
+from django_oauth_toolkit.views import ApiEndpoint
+
+oauth2_endpoint_views = [
+    path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    path('token', oauth2_views.TokenView.as_view(), name="token"),
+    path('revoke-token', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
+]
+
+if settings.DEBUG:
+    # OAuth2 Application Management endpoints
+    oauth2_endpoint_views += [
+        path('applications', oauth2_views.ApplicationList.as_view(), name="list"),
+        path('applications/register', oauth2_views.ApplicationRegistration.as_view(), name="register"),
+        path('applications/<int:pk>', oauth2_views.ApplicationDetail.as_view(), name="detail"),
+        path('applications/<int:pk>/delete', oauth2_views.ApplicationDelete.as_view(), name="delete"),
+        path('applications/<int:pk>/update', oauth2_views.ApplicationUpdate.as_view(), name="update"),
+    ]
+
+    # OAuth2 Token Management endpoints
+    oauth2_endpoint_views += [
+        path('authorized-tokens', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
+        path('authorized-tokens/<int:pk>/delete', oauth2_views.AuthorizedTokenDeleteView.as_view(),
+             name="authorized-token-delete"),
+    ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('o/', include((oauth2_endpoint_views, "django_oauth_toolkit"), namespace='oauth2_provider')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('api/hello', ApiEndpoint.as_view())
 ]
