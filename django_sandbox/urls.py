2f9ba04c599d763dc2321da662e24c319e7d4146
ViggieSmalls <victormovileanu@gmail.com>
2019-11-01 12:00:38 +0100

    step 1: create draggable elements

--- django_sandbox/urls.py
+++ django_sandbox/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from drag_and_drop.views import IndexView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view())
 ]
